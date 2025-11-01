581795749fa058e54c5e7bbb5d237938dffb062d
bikshu-builds <bikshutulimelli@gmail.com>
2025-11-02 01:09:13 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://earbuddies-1.onrender.com/api",
 });
 
 // Auto attach JWT to every request
